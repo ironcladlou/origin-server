b9ff2f487a4c10bf07e71f4a9bc0d6e45e89fd93
Dan Mace <ironcladlou@gmail.com>
2013-07-23 12:05:54 -0400

    WIP upgrade refactors

--- node/lib/openshift-origin-node/utils/upgrade_progress.rb
+++ node/lib/openshift-origin-node/utils/upgrade_progress.rb
@@ -1,6 +1,4 @@
 require 'openshift-origin-common/utils/path_utils'
-require 'sqlite3'
-require 'securerandom'
 
 require_relative 'selinux'
 
@@ -8,20 +6,14 @@ module OpenShift
   module Runtime
     module Utils
       class UpgradeProgress
-        attr_reader :gear_home, :gear_base_dir, :uuid, :hostname, :version
+        attr_reader :gear_home, :gear_base_dir, :uuid, :steps
 
-        module EventType
-          EVENT = "EVENT"
-          IRREGULAR_EXIT = "IRREGULAR_EXIT"
-        end
-
-        def initialize(gear_base_dir, gear_home, uuid, hostname, version)
+        def initialize(gear_base_dir, gear_home, uuid)
           @gear_base_dir = gear_base_dir
           @gear_home = gear_home
           @uuid = uuid
-          @hostname = hostname
-          @version = version
           @buffer = []
+          @steps = {}
         end
 
         def init_store
@@ -35,91 +27,31 @@ module OpenShift
             mcs_label = OpenShift::Runtime::Utils::SELinux::get_mcs_label(uuid)
             OpenShift::Runtime::Utils::SELinux.set_mcs_label_R(mcs_label, runtime_dir)
           end
-
-          upgrade_db_base_dir = File.join(gear_base_dir, '.upgrades')
-          FileUtils.mkdir(upgrade_db_base_dir, mode: 0o640) unless Dir.exists?(upgrade_db_base_dir)
-
-          upgrade_db_path = File.join(upgrade_db_base_dir, "upgrade-#{version}.sqlite")
-
-          if !File.exists?(upgrade_db_path)
-            begin
-              @db = SQLite3::Database.new upgrade_db_path
-              FileUtils.chmod(0o640, upgrade_db_path)
-
-              # Create a database
-              rows = @db.execute <<-EOF
-                create table event (
-                  event_id varchar(36) primary key not null,
-                  event_seq integer,
-                  gear_uuid text not null,
-                  hostname text not null,
-                  name text not null,
-                  type text check (type in ('EVENT', 'IRREGULAR_EXIT')),
-                  timestamp datetime default(strftime('%Y-%m-%d %H:%M:%f', 'NOW')),
-                  rc integer,
-                  stdout varchar(1024),
-                  stderr varchar(1024)
-                );
-              EOF
-            rescue => e
-              raise "Failed to initialize upgrade db at #{upgrade_db_path}: #{e.message}"
-            end
-          else
-            begin
-              @db = SQLite3::Database.new upgrade_db_path
-            rescue => e
-              raise "Failed to open existing upgrade db at #{upgrade_db_path}: #{e.message}"
-            end
-          end
         end
 
-        def add_event(name, event_args = {})
-          event = {
-            :event_id => SecureRandom.uuid.delete('-'),
-            :gear_uuid => uuid,
-            :hostname => hostname,
-            :name => name,
-            :type => event_args[:type] || EventType::EVENT,
-            :rc => event_args[:rc],
-            :stdout => event_args[:stdout],
-            :stderr => event_args[:stderr]
-          }
+        def step(name)
+          unless @steps.has_key?(name)
+            @steps[name] = {
+              :status => complete?(name) ? :complete : :incomplete,
+              :errors => [],
+              :context => {}
+            }
+          end
 
-          insert = <<-EOF
-            insert into event (event_id,
-              event_seq,
-              gear_uuid,
-              hostname,
-              name,
-              type,
-              rc,
-              stdout,
-              stderr
-            ) values (
-              :event_id,
-              (select max(ifnull(event_seq, 0)) + 1 from event),
-              :gear_uuid,
-              :hostname,
-              :name,
-              :type,
-              :rc,
-              :stdout,
-              :stderr
-            )
-          EOF
+          step = @steps[name]
 
-          begin
-            @db.execute(insert,
-              "event_id" => event[:event_id],
-              "gear_uuid" => event[:gear_uuid],
-              "hostname" => event[:hostname],
-              "name" => event[:name],
-              "type" => event[:type].to_s,
-              "rc" => event[:rc],
-              "stdout" => event[:stdout],
-              "stderr" => event[:stderr])
-          rescue => e
-            @buffer << "Failed to insert sqlite upgrade event: #{e.message}\n#{event.inspect}"
+          if incomplete?(name)
+            begin
+              yield(step[:context], step[:errors])
+            rescue OpenShift::Runtime::Utils::ShellExecutionException => e
+              steps[:errors] << "Unhandled shell exception performing step: #{e.message}\nreturn code: #{e.rc}\nstdout: #{e.stdout}\nstderr: #{e.stderr}"
+              raise e
+            rescue => e
+              step[:errors] << "Unhandled exception performing step: #{e.message}\n#{e.backtrace.join("\n")}"
+              raise e
+            end
+
+            mark_complete(name) if step[:errors].empty?
           end
         end
 
@@ -134,6 +66,7 @@ module OpenShift
         def mark_complete(marker)
           IO.write(marker_path(marker), '')
           log "Marking step #{marker} complete"
+          @steps[marker][:status] = :complete
         end
 
         def has_instruction?(instruction)
@@ -164,9 +97,6 @@ module OpenShift
         end
 
         def log(string, event_args = {})
-          add_event(string, event_args)
-
-          string = "#{uuid} #{string}"
           if event_args.has_key?(:rc) || event_args.has_key?(:stdout) || event_args.has_key?(:stderr)
             string = "#{string}\nrc: #{event_args[:rc]}\nstdout: #{event_args[:stdout]}\nstderr: #{event_args[:stderr]}"
           end
