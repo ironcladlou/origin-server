a8a9e8c72d706391dd750a2930ead431e8630022
Dan Mace <ironcladlou@gmail.com>
2013-08-28 10:43:38 -0400

    Hackday

--- node/lib/openshift-origin-node/upgrade/upgrade_machine.rb
+++ node/lib/openshift-origin-node/upgrade/upgrade_machine.rb
@@ -3,7 +3,7 @@ require 'state_machine'
 require 'stomp'
 require 'mongoid'
 
-require cluster_scanner
+require_relative 'cluster_scanner'
 
 module OpenShift
   module Runtime
@@ -87,21 +87,32 @@ module OpenShift
         end
 
         def create_execution(target_version, max_attempts)
-          execution = UpgradeExecution.new(target_version: target_version)
+          execution = UpgradeExecution.find_by(target_version: target_version)
 
-          find_gears_to_upgrade.each do |gear|
-            execution << GearMachine.new(uuid: gear[:uuid], node: gear[:node], target_version: target_version, max_attempts: max_attempts, active: gear[:active])
+          if execution
+            puts "Reusing existing execution for version #{target_version}"
+          else
+            puts "Creating new execution for version #{target_version}"
+
+            execution = UpgradeExecution.create(target_version: target_version)
+
+            find_gears_to_upgrade.each do |gear|
+              execution.gear_machines << GearMachine.create(uuid: gear[:uuid], node: gear[:node], target_version: target_version, max_attempts: max_attempts, active: gear[:active])
+            end
           end
+
+          execution
         end
 
-        def find_gears_to_upgrade
+        def find_gears_to_upgrade # replace w/ ClusterScanner for real use
           gears = []
-          (1..5) do |node_count|
-            (1..10) do |gear_count|
+          (1..5).each do |node_count|
+            (1..10).each do |gear_count|
               gears << { uuid: "uuid#{node_count}-#{gear_count}", name: "gear#{node_count}-#{gear_count}",
                          app_name: "app#{node_count}-#{gear_count}", node: "node#{node_count}", login: "login#{node_count}-#{gear_count}", active: true }
             end
           end
+          puts "synthesized #{gears.length} gears"
           gears
         end
       end
@@ -113,27 +124,6 @@ Mongoid.load!("mongoid.yml")
 
 include OpenShift::Runtime::Upgrade
 
+coord = Coordinator.new
 
-
-execution.gears << OpenShift::Runtime::Upgrade::GearMachine.new(uuid: "db054b6", node: "ip-1-2-3-4", target_version: '2.0.31', max_attempts: 2)
-
-puts "initial gear state: #{gear.state}"
-puts "initial gear results: #{gear.upgrade_results.inspect}"
-puts "initia gear: #{gear.max_attempts} #{gear.num_attempts}"
-
-puts "calling upgrade"
-
-gear.upgrade
-puts "post-upgrade call state: #{gear.state}"
-
-puts "calling complete upgrade"
-gear.complete_upgrade(OpenShift::Runtime::Upgrade::UpgradeResult.new(upgrade_errors: ['error 1', 'error 2'], remote_results: { remote_key_1: 'val1', remote_key_2: 'val2'}))
-
-puts "post-complete upgrade state: #{gear.state}"
-puts "post-complete upgrade results: #{gear.upgrade_results.inspect}"
-
-puts "calling another complete upgrade"
-gear.complete_upgrade(OpenShift::Runtime::Upgrade::UpgradeResult.new(upgrade_errors: ['error 3', 'error 4'], remote_results: { remote_key_3: 'val3', remote_key_4: 'val4'}))
-
-puts "post-complete upgrade state: #{gear.state}"
-puts "post-complete upgrade results: #{gear.upgrade_results.inspect}"
+coord.create_execution('2.0.31', 2)
